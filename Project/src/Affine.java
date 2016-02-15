6992d52ea33a73c44390de432e85b1b1b8a905a4
Isaiah Fuller <fuller.spectrum@gmail.com>
2016-02-15 10:47:46 -0500

    Update Affine.java
    Added decrypt, file input

--- Project/src/Affine.java
+++ Project/src/Affine.java
@@ -7,10 +7,13 @@ public class Affine {
 		int k1 = kb.nextInt();
 		System.out.print("Key 2: ");
 		int k2 = kb.nextInt();
-		//String pt = read(kb.nextLine()); //testing
+		kb.nextLine();
+		System.out.print("File to encrypt: ");
+		String fileName = read(kb.nextLine()); //testing
 		kb.close();
-		
-		System.out.println("Ciphertext: " + encrypt(k1, k2, "spring"));
+		String ct = encrypt(k1, k2, fileName);
+		System.out.println("Ciphertext: " + ct);
+		System.out.println("Plaintext:  " + decrypt(k1, k2, ct));
 	}
 	public static String read(String fileName){
 		Scanner file = null;
@@ -18,25 +21,45 @@ public class Affine {
 		try{
 			file = new Scanner(new FileInputStream(fileName));
 		}catch(FileNotFoundException e){
-			System.out.println("File  was not found");
-			System.out.println("or could not be opened.");
+			System.out.println("File was not found or could not be opened.");
 			System.exit(0);
 		}
-		while(file.hasNext()){
-			if(file.next() != " ")
-				plaintext.concat(file.next());
+		while(file.hasNextLine()){
+			String temp = file.nextLine();
+			temp += "\n";
+			plaintext += temp;
 		}
+		file.close();
 		return plaintext;
 	}
 	public static String encrypt(int k1, int k2, String pt){
+		pt = pt.toLowerCase();
 		String ciphertext = "";
 		for(int i=0; i<pt.length(); i++){
 			char temp = pt.charAt(i);
-			if(temp != ' '){
+			if(Character.isLetter(temp)){
 				temp = (char) ((k1 * (temp - 'a') + k2) % 26 + 'a');
 			}
 			ciphertext += temp;
 		}
 		return ciphertext;
+	}
+	public static String decrypt(int k1, int k2, String ct){
+		ct = ct.toLowerCase();
+		String plaintext = "";
+		int k1inv = 0;
+		int tempi = 0;
+		for(int i=0; i<26; i++){
+			tempi = (k1 * i) % 26;
+			if(tempi == 1)
+				k1inv = i;
+		}
+		for(int i=0; i<ct.length(); i++){
+			char temp = ct.charAt(i);
+			if(Character.isLetter(temp))
+				temp = (char) ((k1inv * (temp - 'a' - k2 + 26)) % 26 + 'a');
+			plaintext += temp;
+		}
+		return plaintext;
 	}
 }
