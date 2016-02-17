e0aa13471d033ee38d2a7c6e555a1ac604b3be27
Remeil <stilesjohnathan21@gmail.com>
2016-02-16 21:00:05 -0500

    Add methods to compute relative frequency of letters.

--- Project/src/Analysis.java
+++ Project/src/Analysis.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
+import java.util.Map.Entry;
 
 /*
  * Analysis.java
@@ -24,17 +25,82 @@ public class Analysis {
 	while (plaintextToAnalyze.equals("")) {
 	    System.out.print("Plaintext file: ");
 	    try {
-		plaintextToAnalyze = FileOps.readFile(scan.nextLine());
+		plaintextToAnalyze = FileOps.readFile(scan.nextLine()).toUpperCase();
 	    }
 	    catch (IOException e){}
 	}
-	
+
 	while (ciphertextToAnalyze.equals("")) {
 	    System.out.print("Ciphertext file: ");
 	    try {
-		ciphertextToAnalyze = FileOps.readFile(scan.nextLine());
+		ciphertextToAnalyze = FileOps.readFile(scan.nextLine()).toUpperCase();
 	    }
 	    catch (IOException e){}
 	}
+    }
+
+    private static HashMap<String, Double> ComputeRelativeFrequencyLetters(String text) {
+	long length = text.length();
+	HashMap<String, Integer> freqMap = new HashMap<String, Integer>();
+
+	for (int i = 0; i < length; i++) {
+	    char character = text.charAt(i);
+	    
+	    if ((character >= 'A' && character <= 'Z') || character == ' ')
+	    {
+		int freq = freqMap.get(character) + 1;
+		freqMap.put(Character.toString(character), freq);
+	    }
+	}
+
+	HashMap<String, Double> relativeFreqMap = new HashMap<String, Double>();
+	long totalFreq = 0;
+	
+	for (Entry<String, Integer> entry : freqMap.entrySet()) {
+	    totalFreq += entry.getValue();
+	}
+	
+	for (Entry<String, Integer> entry : freqMap.entrySet()) {
+	    double relativeFreq = (double)entry.getValue() / (double)totalFreq;
+	    relativeFreqMap.put(entry.getKey(), relativeFreq);
+	}
+	
+	return relativeFreqMap;
+    }
+    
+    private static HashMap<String, Double> ComputeRelativeFrequencyDigraphs(String text) {
+	long length = text.length();
+	
+	if (length <= 1) {
+	    return new HashMap<String, Double>();
+	}
+	
+	HashMap<String, Integer> freqMap = new HashMap<String, Integer>();
+
+	for (int i = 0; i < length - 1; i++) {
+	    char character1 = text.charAt(i);
+	    char character2 = text.charAt(i+1);
+	    
+	    if (((character1 >= 'A' && character1 <= 'Z') || character1 == ' ') &&
+	       ((character2 >= 'A' && character2 <= 'Z') || character2 == ' '))
+	    {
+		int freq = freqMap.get(character1 + character2) + 1;
+		freqMap.put(("" + character1 + character2), freq);
+	    }
+	}
+
+	HashMap<String, Double> relativeFreqMap = new HashMap<String, Double>();
+	long totalFreq = 0;
+	
+	for (Entry<String, Integer> entry : freqMap.entrySet()) {
+	    totalFreq += entry.getValue();
+	}
+	
+	for (Entry<String, Integer> entry : freqMap.entrySet()) {
+	    double relativeFreq = (double)entry.getValue() / (double)totalFreq;
+	    relativeFreqMap.put(entry.getKey(), relativeFreq);
+	}
+	
+	return relativeFreqMap;
     }
 }
